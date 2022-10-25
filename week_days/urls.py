b2eb35ab4dff33c990945267fb3bb88f3d1911bb
Natalia Popova <sverdlova3@gmail.com>
2022-10-25 14:56:30 +0500

    dinamic urls in week_days

--- week_days/urls.py
+++ week_days/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('monday/', views.monday),
-    path('tuesday/', views.tuesday),
+    # path('monday/', views.monday),
+    # path('tuesday/', views.tuesday),
+    path('<days>/', views.get_week_days),
 ]
--- week_days/views.py
+++ week_days/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+
 
 # Create your views here.
-def monday(request):
-    return HttpResponse("Понедельник день тяжелый")
+def get_week_days(request, days):
+    day_list = {
+        'monday': 'Пн - работать',
+        'tuesday': 'Вт - учиться',
+        'wednesday': 'Ср - гладить котов',
+        'thursday': 'Чт - питон учить',
+        'friday': 'Пт - пиво пить',
+        'saturday': 'Сб - болеть',
+        'sunday': 'Вс - в лес ходить',
+    }
+    return HttpResponse(day_list.get(days, f'Не найден день недели {days}'))
 
-def tuesday(request):
-    return HttpResponse("Вторник: надо пить валерьянку")
+# def monday(request):
+#     return HttpResponse("Понедельник день тяжелый")
+#
+# def tuesday(request):
+#     return HttpResponse("Вторник: надо пить валерьянку")
