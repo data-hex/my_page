4ba2554b5fa37a7487fd4af4bcfdf95ea4d38483
Natalia Popova <sverdlova3@gmail.com>
2022-11-07 15:54:51 +0500

    add calculate area

--- geometry/views.py
+++ geometry/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+from math import pi
+
 
 # Create your views here.
+def get_rectangle_area(request, width: int, height: int):
+    area = width * height
+    answer = f"Площадь прямоугольника размером {width}x{height} равна {area}"
+    return HttpResponse(answer)
+
+
+def get_square_area(request, side: int):
+    area = side ** 2
+    answer = f"Площадь квадрата размером {side}x{side} равна {area}"
+    return HttpResponse(answer)
+
+
+def get_circle_area(request, radius: int):
+    area = pi * radius ** 2
+    answer = f"Площадь круга радиуса {radius} равна {area}"
+    return HttpResponse(answer)
