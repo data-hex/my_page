b18047284bbbe788d5348b346b6ff6f2cc91a317
Natalia Popova <sverdlova3@gmail.com>
2022-11-09 15:22:31 +0500

    add redirect

--- week_days/views.py
+++ week_days/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 
 # Create your views here.
-def get_week_days(request, days: str):
-    day_list = {
+day_list = {
         'monday': 'Пн - работать',
         'tuesday': 'Вт - учиться',
         'wednesday': 'Ср - гладить котов',
@@ -13,11 +12,16 @@ def get_week_days(request, days: str):
         'saturday': 'Сб - болеть',
         'sunday': 'Вс - в лес ходить',
     }
+
+
+def get_week_days(request, days: str):
     return HttpResponse(day_list.get(days, f'Не найден день недели {days}'))
 
 
 def get_week_days_by_number(request, days: int):
     if days in range(1, 8):
-        return HttpResponse(f"Сегодня {days} день недели")
+        names = list(day_list)
+        name_day = names[days - 1]
+        return HttpResponseRedirect(f"/todo_week/{name_day}")
     else:
         return HttpResponseNotFound(f"Неверный номер дня - {days}")
