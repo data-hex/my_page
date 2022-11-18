8a6e4c4201be159192fe0abfb62f09c60318c52d
Natalia Popova <sverdlova3@gmail.com>
2022-11-18 14:07:21 +0500

    add reverse

--- geometry/urls.py
+++ geometry/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('rectangle/<int:width>/<int:height>', views.get_rectangle_area),
-    path('square/<int:side>/', views.get_square_area),
-    path('circle/<int:radius>/', views.get_circle_area),
-    path('get_rectangle_area/<int:width>/<int:height>', views.rectangle_area),
-    path('get_square_area/<int:side>/', views.square_area),
-    path('get_circle_area/<int:radius>/', views.circle_area),
+    path('rectangle/<int:width>/<int:height>', views.get_rectangle_area, name='name-rectangle'),
+    path('square/<int:side>/', views.get_square_area, name='name-square'),
+    path('circle/<int:radius>/', views.get_circle_area, name='name-circle'),
+    path('get_rectangle_area/<int:width>/<int:height>', views.get_rectangle_area),
+    path('get_square_area/<int:side>/', views.get_square_area),
+    path('get_circle_area/<int:radius>/', views.get_circle_area),
 ]
--- geometry/views.py
+++ geometry/views.py
@@ -1,34 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from math import pi
+from django.urls import reverse
 
 
 # Create your views here.
 def get_rectangle_area(request, width: int, height: int):
+    if 'get_' in request.path:
+        redirect_url = reverse('name-rectangle', args=(width, height))
+        return HttpResponseRedirect(redirect_url)
     area = width * height
     answer = f"Площадь прямоугольника размером {width}x{height} равна {area}"
     return HttpResponse(answer)
 
 
-def rectangle_area(request, width: int, height: int):
-    return HttpResponseRedirect(f"/calculate_geometry/rectangle/{width}/{height}")
-
-
 def get_square_area(request, side: int):
+    if 'get_' in request.path:
+        redirect_url = reverse('name-square', args=[side])
+        return HttpResponseRedirect(redirect_url)
     area = side ** 2
     answer = f"Площадь квадрата размером {side}x{side} равна {area}"
     return HttpResponse(answer)
 
 
-def square_area(request, side: int):
-    return HttpResponseRedirect(f"/calculate_geometry/square/{side}")
-
-
 def get_circle_area(request, radius: int):
+    if 'get_' in request.path:
+        redirect_url = reverse('name-circle', args=[radius])
+        return HttpResponseRedirect(redirect_url)
     area = pi * radius ** 2
     answer = f"Площадь круга радиуса {radius} равна {area}"
     return HttpResponse(answer)
-
-
-def circle_area(request, radius: int):
-    return HttpResponseRedirect(f"/calculate_geometry/circle/{radius}")
