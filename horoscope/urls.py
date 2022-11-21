141a61964a74825113177ba75d13e4d5218348d7
Natalia Popova <sverdlova3@gmail.com>
2022-11-21 14:39:32 +0500

    add elements (stihii)

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index),
+    path('<str:type>/', views.get_elements),
+    path('type/<str:types_zodiac>', views.get_sign_of_elements, name='element-name'),
     path('<int:sign_of_zodiac>', views.get_horoscope_by_sign_by_number),
     path('<str:sign_of_zodiac>', views.get_horoscope_by_sign, name="horoscope-name"),
 ]
--- horoscope/views.py
+++ horoscope/views.py
@@ -20,11 +20,65 @@ zodiac_dict = {
     "pisces": "Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта)."
 }
 
+zodiac_elements_dict = {
+    'fire': ['aries', 'leo', 'sagittarius'],
+    'earth': ['taurus', 'virgo', 'capricorn'],
+    'air': ['gemini', 'libra', 'aquarius'],
+    'water': ['cancer', 'scorpio', 'pisces']
+}
+
+
+def index(request):
+    zodiacs = list(zodiac_dict)
+    li_elem = ''
+    for sign in zodiacs:
+        redirect_path = reverse(viewname='horoscope-name', args=[sign])
+        li_elem += f"<li> <a target='_blank' href='{redirect_path}'>{sign.title()} </a> </li>"
+    response = f"""
+    <ol>
+        {li_elem}
+    </ol>
+    """
+    return HttpResponse(response)
+
+
+def get_sign_of_elements(request, types_zodiac: str):
+    if zodiac_elements_dict.get(types_zodiac):
+        li_elem = ''
+        for element in list(zodiac_elements_dict.get(types_zodiac)):
+            redirect_path = reverse('horoscope-name', args=[element])
+            li_elem += f"<li> <a target='_blank' href='{redirect_path}'>{element.title()} </a> </li>"
+
+        response = f"""
+        <ol>
+            {li_elem}
+        </ol>
+        """
+        return HttpResponse(response)
+    elif zodiac_dict.get(zodiac_elements_dict):
+        return HttpResponse(zodiac_dict[zodiac_elements_dict])
+    else:
+        return HttpResponseNotFound(f"Неверная стихия - {zodiac_elements_dict}")
+
+
+def get_elements(request, type: str):
+    zodiac_elements = list(zodiac_elements_dict.keys())
+    li_elem = ''
+    for element in zodiac_elements:
+        redirect_path = reverse(viewname='element-name', args=[element])
+        li_elem += f"<li> <a target='_blank' href='{redirect_path}'>{element.title()} </a> </li>"
+    response = f"""
+    <ol>
+        {li_elem}
+    </ol>
+    """
+    return HttpResponse(response)
+
 
 def get_horoscope_by_sign(request, sign_of_zodiac: str):
     description = zodiac_dict.get(sign_of_zodiac, None)
     if description:
-        return HttpResponse(description)
+        return HttpResponse(f"<h2>{description}</h2>")
     else:
         return HttpResponseNotFound(f"Неизвестный знак зодиака {sign_of_zodiac}")
 
@@ -33,6 +87,6 @@ def get_horoscope_by_sign_by_number(request, sign_of_zodiac: int):
     zodiacs = list(zodiac_dict)
     if sign_of_zodiac > len(zodiacs):
         return HttpResponseNotFound(f'Неправильный номер Зодиака - {sign_of_zodiac}')
-    name_zodiac = zodiacs[sign_of_zodiac-1]
+    name_zodiac = zodiacs[sign_of_zodiac - 1]
     redirect_url = reverse("horoscope-name", args=[name_zodiac])
     return HttpResponseRedirect(redirect_url)
