2b374982250dc2ad5fc8f61343a7182e710d3acc
Natalia Popova <sverdlova3@gmail.com>
2022-12-05 14:09:41 +0500

    add name horoscope-index

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='horoscope-index'),
     path('<str:type>/', views.get_elements),
     path('type/<str:types_zodiac>', views.get_sign_of_elements, name='element-name'),
     path('<int:sign_of_zodiac>', views.get_horoscope_by_sign_by_number),
